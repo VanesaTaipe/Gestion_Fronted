45fab6a48b74ca6b60c77413ea51753d421ee18a
VanesaTaipe <vanesa.morales@upch.pe>
2025-11-22 20:51:25 -0500

    url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
  
-  phpApiUrl: '',
+  phpApiUrl: 'https://tidbcloud.com/clusters/10418492814063781696/overview?orgId=1372813089209288329&projectId=1372813089454606263',
   
   // Tu backend Python en Render (FastAPI)
   pythonApiUrl: ''

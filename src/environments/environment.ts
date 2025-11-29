72be21c6816249c7e4c9d662776cd17cdd0375c7
VanesaTaipe <vanesa.morales@upch.pe>
2025-11-29 17:56:19 -0500

    A

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,25 @@
+//export const environment = {
+  //production: false,
+  //apiBase: 'http://localhost:8000/api',
+  //apiUrl: 'http://localhost:8000/api'
+//};//
+const isLocal = window.location.hostname === 'localhost' || 
+                window.location.hostname === '127.0.0.1';
+
 export const environment = {
-  production: false,
-  apiBase: 'http://localhost:8000/api',
-  apiUrl: 'http://localhost:8000/api'
+  production: !isLocal,
+  
+  apiBase: isLocal 
+    ? 'http://localhost:8000/api' 
+    : 'https://gestorphp.onrender.com/api',
+  
+  apiUrl: isLocal 
+    ? 'http://localhost:8000/api' 
+    : 'https://gestorphp.onrender.com/api',
+  
+
+  
+  pythonApiUrl: isLocal 
+    ? 'http://localhost:8080/api' 
+    : 'https://upch-2do-backend-python-realworld.onrender.com/api'
 };
